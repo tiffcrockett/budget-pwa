a4309c86aa14e58462209f6b9720e1c9ad0e4043
Tiffany Crockett <crockett.tiff@gmail.com>
2021-03-28 11:48:32 -0700

    add service worker

--- webpack.config.js
+++ webpack.config.js
@@ -8,33 +8,27 @@ const config = {
     path: __dirname + "./public/dist",
     filename: "bundle.js"
   }, 
-  // bundle files on production, not while in dev
-  mode: "productions"
-}; 
+  // bundle files on production
+  mode: "production",
  
-plugins: [
-  new WebpackPwaManifest({
+  plugins: [
+  new WebpackPwaManifest({ 
+    fingerpring: false,
     name: 'budget-pwa',
     short_name: 'budget-pwa',
     description: 'A budget tracking progressive web app.',
     background_color: '#fff',
-    crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+    start_url: "/",
+    display: "standalone",
     icons: [
       {
-        src: path.resolve('src/assets/icon.png'),
+        src: path.resolve('public/icons/icon-192x192.png'),
         sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
-      },
-      {
-        src: path.resolve('src/assets/large-icon.png'),
-        size: '1024x1024' // you can also use the specifications pattern
-      },
-      {
-        src: path.resolve('src/assets/maskable-icon.png'),
-        size: '1024x1024',
-        purpose: 'maskable'
       }
+      
     ]
   })
-]
+]}; 
+
 
 module.exports = config;
