9f1feab24f9537e2b1be6ecc47b53c210f0100d3
Tiffany Crockett <crockett.tiff@gmail.com>
2021-03-30 07:24:01 -0700

    maskable icon

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 4050;
+const PORT = process.env.PORT || 4060;
 
 const app = express();
 
