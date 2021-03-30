d9a42d5c5e1b417c3613c08429a616f4270395dd
Tiffany Crockett <crockett.tiff@gmail.com>
2021-03-30 10:57:16 -0700

    update svc worker again x3

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,7 +13,8 @@ const FILES_TO_CACHE = [
   const PRECACHE = 'precache-v1';
   const DATA_CACHE = 'runtime';
   
-  // self reference to confirm installatio of this svc wkr
+  // self reference to confirm installation of this svc wkr 
+  // svc wkr open precache and beging caching Files To Cache
   self.addEventListener('install', (event) => {
     event.waitUntil(
       caches
@@ -23,7 +24,8 @@ const FILES_TO_CACHE = [
     );
   });
   
-  // The activate handler takes care of cleaning up old caches.
+  // The activate handler takes care of cleaning up old caches
+  // Once the service worker has been activated, it performs the following
   self.addEventListener('activate', (event) => {
     const currentCaches = [PRECACHE, DATA_CACHE];
     event.waitUntil(
@@ -42,20 +44,22 @@ const FILES_TO_CACHE = [
         .then(() => self.clients.claim())
     );
   });
-  // sw hears fetch request, records action in cache
+  // When service worker encouters a fetch request, records action in cache 
+  // this code uses Native JS fetch event 
+  // fetch transaction, respond with cache...
   self.addEventListener('fetch', (event) => {
     if (event.request.url.includes("/api/transction")) {
       event.respondWith(
         caches.open(DATA_CACHE).then((cache) => {
           return fetch(event.request)
           .then((response) => {  
-            // response is good, record and store in cache
+            // response is good send 200, put clone of response in cache, send req to browsr
             if (response.status === 200 ){
               cache.put(event.request.url, response.clone());
             }
              return response;
           }) 
-          // network fail-no internet, return cache 
+          // network request fails and cannot fetch, return cache 
           .catch((err) => {
             return cache.match(event.request);
           })
